2d9fecb83f7d60c00f9e1783695b547663069c33
Fares Shreen <faresshreen138@gmail.com>
2026-04-05 13:58:28 +0200

    apply helmet and rate limiter and login with 2FA and message api's

--- src/config/env.sevices.js
+++ src/config/env.sevices.js
@@ -17,3 +17,5 @@ export const refreshTokenSecret = process.env.refreshTokenSecret;
 export const accessTokenSecret = process.env.accessTokenSecret;
 export const Email = process.env.Email;
 export const Password = process.env.Password; 
+export const allowOrigins = process.env.allowOrigins.split(",");
+export const applicationName = process.env.applicationName;
